bdc59ee9fa7548193bf605fd6c54ab1133654922
truongtieulong00 <tieulongtruong00@gmail.com>
2020-09-08 10:34:41 +0700

    updates

--- EnglishApp/app/src/main/java/com/example/englishapp/MainActivity.kt
+++ EnglishApp/app/src/main/java/com/example/englishapp/MainActivity.kt
@@ -9,21 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        btndanhsachtu.setOnClickListener{
-            Toast.makeText(this,"Bạn đã click vào danh sách từ",Toast.LENGTH_LONG).show()
-        }
-        btnluyentap.setOnClickListener{
-            Toast.makeText(this,"Bạn đã click vào luyện tập",Toast.LENGTH_LONG).show()
-        }
-        btnkiemtra.setOnClickListener{
-            Toast.makeText(this,"Bạn đã click vào kiểm tra",Toast.LENGTH_LONG).show()
-        }
-        btntuhangngay.setOnClickListener{
-            Toast.makeText(this,"Bạn đã click vào từ hàng ngày",Toast.LENGTH_LONG).show()
-        }
-        btnbatcauhoingaunhien.setOnClickListener{
-            Toast.makeText(this,"Bạn đã click vào câu hỏi ngẫy nhiên",Toast.LENGTH_LONG).show()
-        }
 
     }
 
